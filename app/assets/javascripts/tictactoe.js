284021b96292301bbe2b003a958cda6881c09bac
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-05 19:56:35 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,43 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+const winCombo = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+function player(){
+    if(turn % 2 === 0 ){
+      console.log('hello')
+      return "X"
+    } else {
+      return "O"
+    }
+}
+
+function updateState(clickedSquare){
+  $('clickedSquare').text(player())
+}
+
+function setMessage(msgString){
+  $('div#message').text(msgString)
+}
+
+function checkWinner(){
+  var board = [];
+  for(i=0;i<9;i++){
+    board[i] = $(`td#${i}`).val()
+  }
+  for(var el of winCombo){
+    if(board[el[0]] === board[el[1]] && board[el[1]]=== board[el[2]] && board[el[1]]==="X" || board[el[1]]==="O"){
+    setMessage(`Player ${board[el[0]]} won!`)
+    return true
+  }
+  }
+
+}
